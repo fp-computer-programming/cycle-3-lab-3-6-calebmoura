edb0f2ed49ec377c0927e4dd5fd2f5e03eb78828
calebmoura <144838461+calebmoura@users.noreply.github.com>
2023-10-05 04:24:00 -0400

    Add files via upload

---
+++ lab_3-6.py
@@ -0,0 +1,13 @@
+result_string = "Fairfield" + " " + "Prep"
+
+dashed_line = "-" * 20
+
+print("I'm loving this short story I'm reading, \"The Fall of the House of Usher\"")
+
+print(len("I'm loving this short story I'm reading, \"The Fall of the House of Usher\""))
+
+character_l = "apple"[3]
+
+print(result_string)
+print(dashed_line)
+print(character_l)
